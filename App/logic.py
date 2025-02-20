0e170c90f81679ec4b0af1ff36191c417657392a
Juan David <davfore205@gmail.com>
2025-02-19 23:05:53 -0500

    documentos excel y pdf

--- App/logic.py
+++ App/logic.py
@@ -30,7 +30,7 @@ import time
 from DataStructures.List import array_list as lt
 from DataStructures.Queue import queue as q
 from DataStructures.Stack import stack as st
-from DataStructures.List import single_linked_list as sll
+
 # TODO Importar las librerías correspondientes para el manejo de pilas y colas
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
